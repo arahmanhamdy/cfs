d55ca2341c3ed2c6b1cac3107e4416bc111e1874
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2015-10-05 12:10:59 +0200

    fix url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://52.25.251.24/cfs';
 
 /*
 |--------------------------------------------------------------------------
